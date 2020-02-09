9b09d07518751811d35e0aa17a55ed551e105705
Michael <ac2031@hotmail.com>
2020-02-09 20:36:25 +0100

    Fix Websockets beta value

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const debug = false;
 wss.on('connection', ws => {
 	console.log('Client connected');
 	ws.on('message', data => {
+		console.log(data);
 		wss.clients.forEach(client => {
 			client.send(data);
 		});
