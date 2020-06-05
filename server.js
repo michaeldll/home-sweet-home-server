2f2c3c8d832dd9742309b1f1e7e263d3b4fcd4d7
michaeldll <ac2031@hotmail.com>
2020-06-05 14:39:11 +0200

    Test

--- server.js
+++ server.js
@@ -4,7 +4,6 @@
 const express = require('express');
 const PORT = process.env.PORT || 1234;
 const INDEX = '/index.html';
-
 const server = express()
 	.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
 	.listen(PORT, () => console.log(`Listening on ${PORT}`));
